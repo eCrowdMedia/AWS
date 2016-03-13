93cb2a088ed6cbde04fb7ddfc627d498cdaae1ed
Willy <willy.yeh@gmail.com>
2016-03-14 04:22:27 +0800

    Indent using Spaces.

--- Aws_lib.php
+++ Aws_lib.php
@@ -123,20 +123,20 @@ class Aws_lib
     private function _return_bucket_policy($bucket_name = '')
     {
         return '{
-			"Version": "2008-10-17",
-			"Id": "PolicyForCloudFrontPrivateContent",
-			"Statement": [
-				{
-					"Sid": "1",
-					"Effect": "Allow",
-					"Principal": {
-						"AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity '.$this->cfIdentity.'"
-					},
-					"Action": "s3:GetObject",
-					"Resource": "arn:aws:s3:::'.$bucket_name.'/*"
-				}
-			]
-		}';
+            "Version": "2008-10-17",
+            "Id": "PolicyForCloudFrontPrivateContent",
+            "Statement": [
+                {
+                    "Sid": "1",
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": "arn:aws:iam::cloudfront:user/CloudFront Origin Access Identity '.$this->cfIdentity.'"
+                    },
+                    "Action": "s3:GetObject",
+                    "Resource": "arn:aws:s3:::'.$bucket_name.'/*"
+                }
+            ]
+        }';
     }
 
     /**
